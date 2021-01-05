af0115f907ef54e34488c9e1999708fe207750de
kim <beomsu9696@daum.net>
2021-01-05 15:21:10 +0900

    add text.js

---
+++ .history/text_20210105151302.js
@@ -0,0 +1,16 @@
+export class Text {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    this.canvas.style.position = "absolute";
+    this.canvas.style.left = "0";
+    this.canvas.style.top = "0";
+    document.body.appendChild(this.canvas);
+
+    this.ctx = this.canvas.getContext("2d");
+  }
+
+  setText(str, density, stageWidth, stageHeight) {
+    this.canvas.width = stageWidth;
+    this.canvas.height = stageHeight;
+  }
+}
