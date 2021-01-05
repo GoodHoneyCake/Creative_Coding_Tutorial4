cdb017ca6b60c0f1bdd31806d9193de6e5f271fc
kim <beomsu9696@daum.net>
2021-01-05 16:34:19 +0900

    add particle.js

---
+++ .history/particle_20210105162825.js
@@ -0,0 +1,23 @@
+const FRICTION = 0.98;
+const MOVE_SPEED = 0.88;
+
+export class Particle {
+  constructor(pos, color) {
+    this.color = color;
+    this.maxRadius = Math.random() * (50 - 20) + 20;
+
+    this.savedX = pos.x;
+    this.savedY = pos.y;
+    this.x = pos.x;
+    this.y = pos.y;
+
+    this.progress = 0;
+    this.radius = 0;
+    this.vr = 0;
+    this.vx = 0;
+    this.vy = 0;
+
+    this.fps = 30;
+    this.fpsTime = 1000 / this.fps;
+  }
+}
